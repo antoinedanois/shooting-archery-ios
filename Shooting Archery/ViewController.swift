4bec0793f6126e1043752a0e3891bf49d236bca4
antoine.danoislycee@gmail.com <antoine.danoislycee@gmail.com>
2015-11-13 00:50:59 +0100

    Menu principal et utilisateur fait

--- Shooting Archery/ViewController.swift
+++ Shooting Archery/ViewController.swift
@@ -9,12 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var LabelCenter: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        var coucou
-        var test
+        LabelCenter.textAlignment = NSTextAlignment.Center;
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,6 +25,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    
+    // MARK : Actions
+    
+    
 
 }
 
